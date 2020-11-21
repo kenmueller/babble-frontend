1973a6c58eed826eee9d0037359830cadfa38fe8
Ken Mueller <kenmueller0@gmail.com>
2020-11-20 20:37:16 -0800

    notification click

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,29 @@
 self.addEventListener('push', event => {
-	const { title, body } = event.data.json()
-	self.registration.showNotification(title, { body })
+	const { title, body, url } = event.data.json()
+	
+	event.waitUntil(
+		self.registration.showNotification(title, {
+			body,
+			data: { url }
+		})
+	)
+})
+
+self.addEventListener('notificationclick', event => {
+	const { notification } = event
+	const { url } = notification.data
+	
+	notification.close()
+	event.waitUntil(
+		clients.matchAll({ type: 'window' }).then(_clients => {
+			const client = _clients.find(client =>
+				client.url === url && 'focus' in client
+			)
+			
+			if (client)
+				client.focus()
+			else if (clients.openWindow)
+				clients.openWindow(url)
+		})
+	)
 })
